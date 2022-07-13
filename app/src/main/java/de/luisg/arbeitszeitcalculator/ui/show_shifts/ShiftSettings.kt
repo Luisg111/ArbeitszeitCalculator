4500559c597f5e1c666a257c97e8123e049ceb63
lgutzeit <lgutzeit@stud.hs-heilbronn.de>
2022-07-13 22:37:28 +0200

    added option to hide shift settings

--- app/src/main/java/de/luisg/arbeitszeitcalculator/ui/show_shifts/ShiftSettings.kt
+++ app/src/main/java/de/luisg/arbeitszeitcalculator/ui/show_shifts/ShiftSettings.kt
@@ -1,46 +1,63 @@
 package de.luisg.arbeitszeitcalculator.ui.show_shifts
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 @Preview
 fun createShiftSettings() {
-    var loanValue = remember { mutableStateOf(TextFieldValue("11")) }
-    var maxHoursMonth = remember { mutableStateOf(TextFieldValue("450")) }
+    var loanValue by remember { mutableStateOf(TextFieldValue("11")) }
+    var maxHoursMonth by remember { mutableStateOf(TextFieldValue("450")) }
+    var showSettings by remember { mutableStateOf(false) }
+
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(8.dp)
+            .padding(16.dp)
     ) {
-        TextField(
-            value = loanValue.value,
-            onValueChange = { loanValue.value = it },
-            label = { Text("Stundenlohn (€)") },
-            modifier = Modifier.fillMaxWidth(),
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Number
-            )
+        Text(
+            text = "Schichteinstellungen",
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable {
+                    showSettings = !showSettings
+                }
+
         )
-        Spacer(modifier = Modifier.height(6.dp))
-        TextField(
-            value = maxHoursMonth.value,
-            onValueChange = { maxHoursMonth.value = it },
-            label = { Text("Monatslimit (€)") },
-            modifier = Modifier.fillMaxWidth(),
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Number
+        if (showSettings == true) {
+            Spacer(modifier = Modifier.height(6.dp))
+            TextField(
+                value = loanValue,
+                onValueChange = { loanValue = it },
+                label = { Text("Stundenlohn (€)") },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number
+                )
             )
-        )
+            Spacer(modifier = Modifier.height(6.dp))
+            TextField(
+                value = maxHoursMonth,
+                onValueChange = { maxHoursMonth = it },
+                label = { Text("Monatslimit (€)") },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number
+                )
+            )
+        }
     }
 }
